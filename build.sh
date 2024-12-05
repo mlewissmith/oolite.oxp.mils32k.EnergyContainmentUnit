0bd33c0318ff38fb588ef465c9c8190917679896
mlewissmith <mlewissmith@users.noreply.github.com>
2024-12-05 16:32:37 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ workdir=$(dirname $0)
 builddir=${workdir}/meson.build.d
 rm_builddir=false
 meson_setup_opts=
+xmode="build"
 
 usage() {
     cat<<EOF
@@ -17,25 +18,32 @@ $0 [ -O BUILDDIR ] [ -RCW ] target...
 EOF
 }
 
-while getopts O:RCW opt
+while getopts O:RCWX: opt
 do
     case $opt in
         O) builddir=$OPTARG ;;
         R) rm_builddir=true ;;
         C) meson_setup_opts+="--reconfigure " ;;
         W) meson_setup_opts+="--wipe " ;;
+        X) xmode=$OPTARG ;;
         *) usage ; exit 1 ;;
     esac
 done
 shift $(( ${OPTIND} - 1 ))
-
-################################################################################
-
 cd ${workdir}
-${rm_builddir} && rm -fr ${builddir}
-meson setup --fatal-meson-warnings ${meson_setup_opts} ${builddir}
-meson compile -v -C ${builddir} "$@"
 
 ################################################################################
 
-cp -v -t ${workdir} $(find $builddir -type f -name "*.oxz")
+case $xmode in
+    build)
+        ${rm_builddir} && rm -fr ${builddir}
+        meson setup --fatal-meson-warnings ${meson_setup_opts} ${builddir}
+        meson compile -v -C ${builddir} "$@"
+        cp -v -t ${workdir} $(find $builddir -type f -name "*.oxz")
+        ;;
+    tag) echo -e "$1\n $(git changelog -a -x -t $1)" | git tag -F - $1 ;;
+    *) usage ; exit 1 ;;
+esac
+
+################################################################################
+
